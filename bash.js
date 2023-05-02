dfd5da688867fb3b8289954eac5133d25956fb8b
joel <jejanov@gmail.com>
2023-05-01 19:15:11 -0600

    updates

--- bash.js
+++ bash.js
@@ -1,1 +1,16 @@
+const pwd =  require('./pwd.js')
 // Test commit
+
+//Output a prompt
+process.stdout.write('prompt > ');
+
+// The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();//remove the newline
+  if(cmd === "pwd"){
+   pwd();
+  } else {
+  process.stdout.write("You typed: " + cmd);
+  process.stdout.write("\nprompt > ");
+  }
+});
---
+++ pwd.js
@@ -0,0 +1,6 @@
+
+
+// The stdin 'data' event fires after a user types in a line
+module.exports = function () {
+       return process.stdout.write(process.cwd());
+}
