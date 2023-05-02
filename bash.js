af11bfb9456fd4aa6e4ae627c3dab88984872367
Andrew Ern <shigmiya64@gmail.com>
2023-05-01 21:39:24 -0400

    all saved

--- bash.js
+++ bash.js
@@ -7,15 +7,15 @@ process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim();//remove the newline
-  if(cmd === "pwd"){
-   pwd();
-  }
-  else if(cmd === "ls"){
-    ls();
-  } 
-else{
-    process.stdout.write("You typed: " + cmd);
+    const cmd = data.toString().trim();//remove the newline
+    if(cmd === "pwd"){
+        pwd();
     }
-process.stdout.write("\nprompt > ");
+    else if(cmd === "ls"){
+        ls();
+    } 
+    else{
+        process.stdout.write("You typed: " + cmd);
+    }
+    process.stdout.write("\nprompt > ");
 });
--- pwd.js
+++ pwd.js
@@ -1,5 +1,3 @@
-
-
 // The stdin 'data' event fires after a user types in a line
 module.exports = function () {
        return process.stdout.write(process.cwd());
